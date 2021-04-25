523dbff0fd76ea002ddebdf6b78169c3e3da9633
your name <you@example.com>
2021-04-25 00:00:37 -0400

    direflow bundle update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types'
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"]
       },
+      
       {
         test: /\.js$/,
         exclude: /node_modules/,
